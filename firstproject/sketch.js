0a2d3bcdfbf5ae9fdfa4ccfc415d7b95753e3ec5
dipikaayshics <54912993+dipikaayshics@users.noreply.github.com>
2019-09-06 14:11:50 -0600

    Update sketch.js
    edited new colors

--- firstproject/sketch.js
+++ firstproject/sketch.js
@@ -5,13 +5,28 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let redamount = 0;
+let redchangeamount = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(2);
 }
 
 function draw() {
-  background(220);
-  Fill(0);
-  ell
+  fill(redamount, 0, 255);
+  noStroke();
+  ellipse( mouseX, mouseY, 100, 100);
+
+  redamount += redchangeamount;
+  if (redamount >= 255){
+    redchangeamount *= -1;
+  
+  }
+  if (redamount <= 0){
+    redchangeamount *= -1;
+  }
+  console.log(redamount);
+
 }
+
