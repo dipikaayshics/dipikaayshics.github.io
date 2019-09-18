3c0d83a3c7840f3a70826675e334e8e2235b519e
dipikaayshics <54912993+dipikaayshics@users.noreply.github.com>
2019-09-18 14:11:46 -0600

    tree moves and timer

--- bouncingball/sketch.js
+++ bouncingball/sketch.js
@@ -58,7 +58,7 @@ function draw() {
 function moveshape() {
   x += dx;
   y += dy;
-  
 }
+
   
   
--- interective-scene/sketch.js
+++ interective-scene/sketch.js
@@ -13,8 +13,8 @@ let x;
 let y;
 let treesize = 500
 let dx;
-let dy;
-let s = seconds;
+
+
 
 function preload(){
   tree = loadImage("assets/tree.png");
@@ -36,12 +36,12 @@ function setup() {
   y = height/2;
   
   dx = random (-50, 50);
-  dy = random (-50, 50);
+ 
 }
 
 function draw() {
   displayTree();
-  // bouncetree();
+  bouncetree();
   time();
 
 }
@@ -53,14 +53,24 @@ function bouncetree(){
 }
 function moveshape() {
   x += dx;
-  y += dy;
 }
 function displayTree(){
   imageMode(CENTER);
-  image(tree,x, y, treesize, treesize);
+  image(tree, x, y, treesize, treesize);
 }
 function time (){
   var s = second();
-text('Time Left: \n' + s, 5, 50);
+  textSize(20);
+  frameRate(20);
+  fill(0, 102, 153);
 
+text('Time Left: \n' + s, 5, 50);
+}
+
+function movingBasket() {
+  if (key === LEFT_ARROW){
+    
+
+  }
+  
 }
