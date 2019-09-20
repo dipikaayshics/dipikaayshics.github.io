556c750a50e6722ebf51ef7760561a3aa4a47493
dipikaayshics <dipikaayshi@gmail.com>
2019-09-20 01:52:28 -0600

    added busket and apple are perfectly moving

--- interective-scene/sketch.js
+++ interective-scene/sketch.js
@@ -16,24 +16,28 @@ let treesize = 400;
 let appleSize = 50;
 let dx;
 let dy;
-let treeY = 200;
+let treeY = 300;
+let appleY = [200, 180, 150, 240, 220, 240, 248, 250];
+let busketX = 780;
+let busketY = 780;
 
-let appleY_0 = treeY;
-let appleY_1 = treeY;
-let appleY_2 = treeY;
-let appleY_3 = treeY;
-let appleY_4 = treeY;
-let appleY_5 = treeY;
-let appleY_6 = treeY;
-let appleY_7 = treeY;
-let appleY_8 = treeY;
-let appleY_9 = treeY;
-let appleY_10 = treeY;
+
+// let appleY_0 = treeY;
+// let appleY_1 = treeY;
+// let appleY_2 = treeY;
+// let appleY_3 = treeY;
+// let appleY_4 = treeY;
+// let appleY_5 = treeY;
+// let appleY_6 = treeY;
+// let appleY_7 = treeY;
+// let appleY_8 = treeY;
+// let appleY_9 = treeY;
+// let appleY_10 = treeY;
 
 function preload(){
   tree = loadImage("assets/tree.png");                                  
   bgImage = loadImage("assets/bgImage.jpg");
-  plate = loadImage("assets/plate.png");
+  busket = loadImage("assets/plate.png");
   apple = loadImage("assets/apple.png");
 
 }
@@ -54,6 +58,8 @@ function draw() {
   bouncetree();
   time();
   movingApple();
+  movingBasket()
+  
 
 }
 //showup the tree
@@ -82,30 +88,50 @@ function time (){
   frameRate(20);
   fill(0, 102, 153);
 
-  text('Time Left: \n' + s, 5, 50);
+  text('Time: \n' + s, 5, 50);
   
 }
 
 function movingBasket() {
-  if (key === LEFT_ARROW){
-    }
+  noStroke();
+  imageMode(CENTER);
+  image(busket, busketX, busketY, 100, 100);
+  keyPressed()
+}
+  
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    x = x - 5;
+  } else if (keyCode === RIGHT_ARROW) {
+    x = x + 5;
+  }
+
+    
   }
 
 function movingApple (){
-
+  noStroke();
   
-  imageMode(CENTER);
+  for (var i = 0; i < appleY.length; i++) {
+    image(apple, (i+0.5)*200, appleY[i], appleSize, appleSize);
+    appleY[i] += 6;
+  }
 // all the apples
-  image (apple, 10, appleY_0, appleSize, appleSize);
-  image (apple, 100, appleY_1, appleSize, appleSize);
-  image (apple, 200, appleY_2, appleSize, appleSize);
-  image (apple, 300, appleY_3, appleSize, appleSize);
-  image (apple, 400, appleY_4, appleSize, appleSize);
+  // image (apple, 100, appleY_0, appleSize, appleSize);
+  // image (apple, 200, appleY_1, appleSize, appleSize);
+  // image (apple, 300, appleY_2, appleSize, appleSize);
+  // image (apple, 400, appleY_3, appleSize, appleSize);
+  // image (apple, 500, appleY_4, appleSize, appleSize);
   
-  appleY_0 += 20;
-  appleY_1 += 10;
-  appleY_2 += 10;
+  // appleY_0 += 5;
+  // appleY_1 += 6;
+  // appleY_2 += 7;
+  // appleY_3 += 4;
+  // appleY_4 += 8;
 
 
 }
+
+
+
 
