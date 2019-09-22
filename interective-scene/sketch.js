719e662200f5081fd331e2f61996a6ca96cce929
dipikaayshics <dipikaayshi@gmail.com>
2019-09-22 12:29:16 -0600

    chabngsehfscmvjb,kgjsgd

--- interective-scene/sketch.js
+++ interective-scene/sketch.js
@@ -23,31 +23,6 @@ let appleY = [200, 180, 150, 240, 220, 240, 248, 250];
 let busketX = 780;
 let busketY = 780;
 
-// all the apples
-  // image (apple, 100, appleY_0, appleSize, appleSize);
-  // image (apple, 200, appleY_1, appleSize, appleSize);
-  // image (apple, 300, appleY_2, appleSize, appleSize);
-  // image (apple, 400, appleY_3, appleSize, appleSize);
-  // image (apple, 500, appleY_4, appleSize, appleSize);
-  
-  // appleY_0 += 5;
-  // appleY_1 += 6;
-  // appleY_2 += 7;
-  // appleY_3 += 4;
-  // appleY_4 += 8;
-
-// let appleY_0 = treeY;
-// let appleY_1 = treeY;
-// let appleY_2 = treeY;
-// let appleY_3 = treeY;
-// let appleY_4 = treeY;
-// let appleY_5 = treeY;
-// let appleY_6 = treeY;
-// let appleY_7 = treeY;
-// let appleY_8 = treeY;
-// let appleY_9 = treeY;
-// let appleY_10 = treeY;
-
 function preload(){
   tree = loadImage("assets/tree.png");                                  
   bgImage = loadImage("assets/bgImage.jpg");
@@ -90,18 +65,7 @@ function displayBird(){
   image(bird, x, birdY, birdSize, birdSize);
 }
 
-//bird flying around the tree
-function flyingBird(){
-  birdspeed();
-  if (x > width - birdSize/2 || x < birdSize/2){
-    dx *= -1  
-  }
-}
-//moving the tree in a speed
-function birdspeed() {
-  x += dx;
-  y += dy;
-}
+
 
 function time (){
   var s = second();
@@ -118,9 +82,6 @@ function movingBasket() {
   imageMode(CENTER);
   image(busket, busketX, busketY, busketSize, busketSize);
   
-}
-  
-function keyPressed() {
   if (keyCode === LEFT_ARROW) {
     x = x - 5;
   } else if (keyCode === RIGHT_ARROW) {
@@ -132,26 +93,10 @@ function keyPressed() {
 
 function movingApple (){
   noStroke();
-  
-
   for (let i = 0; i < appleY.length; i++) {
     let appleX = (i+5)*100;
     image(apple, appleX, appleY[i], appleSize, appleSize);
     appleY[i] += 6;
-
-
-// all the apples
-  // image (apple, 100, appleY_0, appleSize, appleSize);
-  // image (apple, 200, appleY_1, appleSize, appleSize);
-  // image (apple, 300, appleY_2, appleSize, appleSize);
-  // image (apple, 400, appleY_3, appleSize, appleSize);
-  // image (apple, 500, appleY_4, appleSize, appleSize);
-  
-  // appleY_0 += 5;
-  // appleY_1 += 6;
-  // appleY_2 += 7;
-  // appleY_3 += 4;
-  // appleY_4 += 8;
 
 }
 }
