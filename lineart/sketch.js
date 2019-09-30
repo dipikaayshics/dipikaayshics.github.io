2e0439d89a968575df6dee707735853feaef22b7
dipikaayshics <dipikaayshi@gmail.com>
2019-09-29 23:08:20 -0600

    started state proj

--- lineart/sketch.js
+++ lineart/sketch.js
@@ -1,46 +1,49 @@
 // Project Title
 // dipika
 // sept 9
-
+let mode = "line";
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
 }
 
 function draw() {
-  line (mouseX, mouseY, pmouseX, pmouseY)
-  
+  if (mode = "line"){
+    line (mouseX, mouseY, pmouseX, pmouseY);
+  }
+  else if (mode = "sizingImg"){
+    // this is the ching shape of an object codes.......
+let tree;
+let scaler = 1;
+function preload(){
+  tree = loadImage("assets/tree.png");
+
 }
-// this is the ching shape of an object codes.......
-// let tree;
-// let scaler = 1;
-// function preload(){
-//   tree = loadImage("assets/tree.png");
 
-// }
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(bgImage);
+}
 
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//   background(bgImage);
-// }
-
-// function draw() {
-//   background(0);
-//   if (keyIsPressed){
-//     if (keyCode === UP_ARROW){
-//       //  increase image size
-//       scaler*= 1.01;
+function draw() {
+  background(0);
+  if (keyIsPressed){
+    if (keyCode === UP_ARROW){
+      //  increase image size
+      scaler*= 1.01;
   
-//     }
-//     else if (keyCode === DOWN_ARROW){
-//       // decrease image size
-//       scaler /= 1.01;
-//     }
-//   }
+    }
+    else if (keyCode === DOWN_ARROW){
+      // decrease image size
+      scaler /= 1.01;
+    }
+  }
 
-//   imageMode(CENTER);
-//   image(tree, mouseX, mouseY, tree.width * scaler, tree.height *scaler)
+  imageMode(CENTER);
+  image(tree, mouseX, mouseY, tree.width * scaler, tree.height *scaler)
 
   
-  
-// }
+  }
+}
+}
+
