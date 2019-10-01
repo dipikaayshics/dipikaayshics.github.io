f4598a5ad654f404cff3f94731b5fe530e1c1bbf
dipikaayshics <54912993+dipikaayshics@users.noreply.github.com>
2019-10-01 14:11:42 -0600

    fifnihed imag pro

--- image-pro/sketch.js
+++ image-pro/sketch.js
@@ -4,29 +4,47 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+let space;
+let filteredImage;
 
-let redamount = 0;
-let redchangeamount = 1;
+function preload() {
+  spaceship = loadImage("assets/spaceship.jpg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(2);
+  filteredImage = makeGrayscale(spaceship);
 }
 
 function draw() {
-  fill(redamount, 0, 255);
-  noStroke();
-  ellipse( mouseX, mouseY, 100, 100);
+  background(220);
 
-  redamount += redchangeamount;
-  if (redamount >= 255){
-    redchangeamount *= -1;
-  
-  }
-  if (redamount <= 0){
-    redchangeamount *= -1;
-  }
-  console.log(redamount);
-
+  imageMode(CENTER);
+  image(filteredImage, mouseX, mouseY);
 }
 
+function makeGrayscale(sourceImage) {
+  let img = createImage(sourceImage.width, sourceImage.height);
+
+  //load pixels so you can adjust them in the array
+  img.loadPixels();
+  sourceImage.loadPixels();
+
+  for (let x = 0; x < sourceImage.width; x++) {
+    for (let y = 0; y < sourceImage.height; y++) {
+      let p = sourceImage.get(x, y);
+
+      //apply filter
+      let r = red(p);
+      let g = green(p);
+      let b = blue(p);
+
+      let average = (r + g + b) / 3;
+
+      img.set(x, y, color(average, average, average));
+    }
+  }
+
+  img.updatePixels();
+  return img;
+}
