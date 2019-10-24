56fe6eb075e89d9be370ef5ea5fa0516a89dbe26
dipikaayshics <54912993+dipikaayshics@users.noreply.github.com>
2019-10-24 13:05:34 -0600

    dah

---
+++ doublearray/sketch.js
@@ -0,0 +1,55 @@
+// Project Title
+// dipika
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let grid = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+ 
+}
+function blah(thegrid){
+  for (let n =0; n< thegrid[0].length; n++){
+    for (let x =0; x< thegrid[0].length; x++){
+      if (thegrid = [n], [x] = 0){
+        fill(255);
+      }
+      else {
+        fill(0);
+      }
+      let cellsize = width/thegrid[0].length;
+      rect(x* cellsize, n*cellsize, cellsize, cellsize);
+  
+    }
+  
+
+
+  }
+
+}
+
+
+function craete2d(cols, rows) {
+  let somearray = {};
+  for (let i = 0; i< cols; i++){
+    somearray.push([]);
+    for (j = o; j< rows; j++){
+      if (random(100< 50){
+        somearray[i].push(1);
+      
+      
+      }
+
+      }
+  }
+  
+}
+return somearray;
+
+
